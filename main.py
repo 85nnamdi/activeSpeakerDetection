9a32cd1ad9c593acaff6e2dc0832251cff3a2b3a
Nnamdi Ekweariri <ekwearirinnamdi@gmail.com>
2021-08-27 11:41:12 +0200

    #implementing distance measure

--- main.py
+++ main.py
@@ -17,35 +17,34 @@ csvDir = 'dataset/csv/train/'
 videos = 'dataset/videos/'
 
 def main():
-    # 1 Download videos
-    videoFile = VideoData(txtvideoSet, Base_train_url)
-    videoFile.download()
+    # # 1 Download videos
+    # videoFile = VideoData(txtvideoSet, Base_train_url)
+    # videoFile.download()
 
     
     util = Utilities()
     rootPath = os.getcwd()
-    # 2 Save video frames
-    list_videos  =util.readFiles(videos,fileExtention='.*')
-    os.chdir(rootPath)
-    list_csv = util.readFiles(basePath = csvDir, fileExtention='.csv')
-    for each_video_file in list_videos:
-        a_vid_file = os.path.basename(each_video_file).split('.',1)[0]
-        for each_csv_file in list_csv:
-            a_csv_file = os.path.basename(each_csv_file).split('.', 1)[0]
-            if a_vid_file in a_csv_file:
-                os.chdir(rootPath)
-                with concurrent.futures.ProcessPoolExecutor() as executor:
-                    executor.submit(util.saveFrame, each_video_file, each_csv_file)
+    # # 2 Save video frames
+    # list_videos  =util.readFiles(videos,fileExtention='.*')
+    # os.chdir(rootPath)
+    # list_csv = util.readFiles(basePath = csvDir, fileExtention='.csv')
+    # for each_video_file in list_videos:
+    #     a_vid_file = os.path.basename(each_video_file).split('.',1)[0]
+    #     for each_csv_file in list_csv:
+    #         a_csv_file = os.path.basename(each_csv_file).split('.', 1)[0]
+    #         if a_vid_file in a_csv_file:
+    #             os.chdir(rootPath)
+    #             with concurrent.futures.ProcessPoolExecutor() as executor:
+    #                 executor.submit(util.saveFrame, each_video_file, each_csv_file)
 
-    # 3 Save keypoint using openpose
-    dirpath = 'dataset/frames/'
-    dir = util.readDir(dirpath)
-    for d in dir[0]:
-        framePath = os.path.join(dirpath,d)
-        output_path =d
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            executor.submit(util.callOpenPose, frames_path ='../'+framePath, output_path='../dataset/json/'+output_path)
-        
+    # # 3 Save keypoint using openpose
+    # dirpath = 'dataset/frames/'
+    # dir = util.readDir(dirpath)
+    # for d in dir[0]:
+    #     framePath = os.path.join(dirpath,d)
+    #     output_path =d
+    #     with concurrent.futures.ProcessPoolExecutor() as executor:
+    #         executor.submit(util.callOpenPose, frames_path ='../'+framePath, output_path='../dataset/json/'+output_path)
 
     util.keyPointToCSV(csvDir=csvDir, jsonDir=jsonPath)
 
